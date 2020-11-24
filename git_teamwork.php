51358125fbd0adc04cef01ef30f827bc03a22d18
Manh <nvmanh@cmc.com.vn>
2020-11-24 19:20:17 +0700

    Commit dau tien cua du an moi

---
+++ git_teamwork.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Code nhóm với Git
+ * - Giảng viên là PM/Trưởng nhóm:
+ * + Tạo respositories của dự án
+ * + Add các member vào
+ * + Là người review code: code mà chưa ổn -> đẩy lại
+ * rm -rf .idea/
+ * cho dev code lại
+ * - Các học viên là các Dev
+ */
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nvmanh
+ * Date: 11/24/2020
+ * Time: 7:16 PM
+ */
+echo "Dự án của lớp PHP0720E";
